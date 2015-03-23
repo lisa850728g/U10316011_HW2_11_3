1d973a08397571f09282be306f7c13882abbb427
lisa850728g <lisa850728g@gmail.com>
2015-03-23 18:30:57 +0800

    update SavingsAccount
    增加註解

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+//inherit the Account class
 class SavingsAccount extends Account{
 	/**private variables*/
 	private int id;
@@ -25,12 +26,14 @@ class SavingsAccount extends Account{
 		//input id and print balance
 		System.out.print("Please enter this saving account's ID: ");
 		id = input.nextInt();
+		//invoke the setId method in Account
 		super.setId(id);
 		setBalance(20000);
 		System.out.println("This saving account have $" + (int)getBalance());
+		//invoke the setAnnualInterestRate method in Account
 		super.setAnnualInterestRate(4.5);
 		
-		//let the user take money but he can't take more than money in account
+		//let the user take money but he can't take more than money in this account
 		do{
 			System.out.print("How much money you want to take ? ");
 			takeMoney = input.nextDouble();
@@ -39,11 +42,13 @@ class SavingsAccount extends Account{
 				System.out.println("You can not take that much money");
 		}while(takeMoney > 20000);
 		
+		//invoke the withdraw method in Account
 		super.withdraw(takeMoney);
 		
 		//let the user store money
 		System.out.print("How much money you want to store ? ");
 		putMoney = input.nextDouble();
+		//invoke the deposit method in Account
 		super.deposit(putMoney);
 		
 		System.out.println();
